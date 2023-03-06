d30495195c88b78fef11f655b534239ad677e5c7
muayad-mahmoud <drakenz2010@gmail.com>
2023-03-07 00:45:28 +0200

    Additional Fixes for url

--- logic/logic.py
+++ logic/logic.py
@@ -133,7 +133,7 @@ class logic:
                                 self.done = True
                                 break
                             try:
-                                user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36 Edg/92.0.902.84"
+                                user_agent = "okhttp/4.9.2 UdemyAndroid 8.9.2(499) (phone)"
                                 Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
                                 options = webdriver.FirefoxOptions()
                                 options.add_argument(
@@ -413,7 +413,7 @@ class logic:
                                 child = r.html.find('span')
                                 self.logger.push(child)
                                 '''
-                                user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36 Edg/92.0.902.84"
+                                user_agent = "okhttp/4.9.2 UdemyAndroid 8.9.2(499) (phone)"
                                 Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
                                 options = webdriver.FirefoxOptions()
                                 options.add_argument(
