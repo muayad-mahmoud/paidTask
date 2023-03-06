47ef34652f6892598c1b1df4da5ce178fc67c832
muayad-mahmoud <drakenz2010@gmail.com>
2023-03-07 00:39:44 +0200

    Aditional Fixess

--- logic/logic.py
+++ logic/logic.py
@@ -133,8 +133,7 @@ class logic:
                                 self.done = True
                                 break
                             try:
-
-                                user_agent = 'Mozilla/5.0' "https://www.udemy.com/courses/search/?q=python&src=sac&kw=python"
+                                user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36 Edg/92.0.902.84"
                                 Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
                                 options = webdriver.FirefoxOptions()
                                 options.add_argument(
@@ -414,7 +413,7 @@ class logic:
                                 child = r.html.find('span')
                                 self.logger.push(child)
                                 '''
-                                user_agent = 'Mozilla/5.0' "https://www.udemy.com/courses/search/?q=python&src=sac&kw=python"
+                                user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36 Edg/92.0.902.84"
                                 Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
                                 options = webdriver.FirefoxOptions()
                                 options.add_argument(
