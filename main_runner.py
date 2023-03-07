060bd099e2688a9fffebe80a07a1a6ce8ca08e0c
muayad-mahmoud <drakenz2010@gmail.com>
2023-03-07 14:00:05 +0200

    change port

--- main_runner.py
+++ main_runner.py
@@ -225,7 +225,7 @@ with ui.column().props('inline color=black').classes('absolute-center justify-ce
 
 if __name__ in {"__main__", "__mp_main__"}:
 
-    ui.run(uvicorn_reload_includes='*.py, *.css, *.html', port=4444)
+    ui.run(uvicorn_reload_includes='*.py, *.css, *.html', port=8080)
     # time_start = datetime.now()
 
     # difference = datetime.now() - time_start
