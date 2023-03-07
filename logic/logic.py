36e0dd3fc26cb88c3ed764a86a4ca36b7951701a
muayad-mahmoud <drakenz2010@gmail.com>
2023-03-07 13:50:32 +0200

    Change binary

--- logic/logic.py
+++ logic/logic.py
@@ -146,7 +146,7 @@ class logic:
                                     "--ignore-certificate-errors")
                                 options.add_argument("--headless")
                                 # options.add_argument('--disable-logging')
-                                options.binary_location = r'C:\\Program Files\\Mozilla Firefox\\firefox.exe'
+                                options.binary_location = r'/usr/bin/firefox'
 
                                 browser = webdriver.Firefox(options=options)
                                 name = each.text
@@ -433,7 +433,7 @@ class logic:
                                     "--ignore-certificate-errors")
                                 options.add_argument("--headless")
                                 # options.add_argument('--disable-logging')
-                                options.binary_location = r'C:\\Program Files\\Mozilla Firefox\\firefox.exe'
+                                options.binary_location = r'/usr/bin/firefox'
 
                                 browser = webdriver.Firefox(options=options)
 
